ad72c8c6380b62c4344f4ec843bd7cad2fb44931
quocbao-pc <trinhngocquocbao@vienthonga.com>
2016-08-25 17:27:36 +0700

    modified:   content/docs-start-installation.md
    modified:   handlebars-helper.js
    
    modified:   layout/document.category.html

--- content/docs-start-installation.md
+++ content/docs-start-installation.md
@@ -9,3 +9,7 @@
     "date": "2016-08-25 17:01:04"
 }
 ---
+<p>Download <a href="{{global.download_link}}">EasyBuider</a>, unzip and run <code>EasyWeb Builder.exe</code> file</p>
+								<p>Sign up / Sign in when EasyBuilder started. This account contains information of websites your owned or managed  </p>
+
+								<p> Notes</p>
--- handlebars-helper.js
+++ handlebars-helper.js
@@ -6,10 +6,10 @@ var moment = require('moment');
 
 module.exports = function (Handlebars) {
     Handlebars.registerHelper(HandlebarsLayouts(Handlebars));
-  //  Helpers({handlebars: Handlebars});
-    ['array', 'code', 'collection', 'comparison', 'date', 'fs', 'html', 'i18n', 'inflection', 'logging', 'markdown', 'match', 'math', 'misc', 'number', 'path', 'string', 'url'].forEach(function(name) {
+    //  Helpers({handlebars: Handlebars});
+    ['array', 'code', 'collection', 'comparison', 'date', 'fs', 'html', 'i18n', 'inflection', 'logging', 'markdown', 'match', 'math', 'misc', 'number', 'path', 'string', 'url'].forEach(function (name) {
         Helpers[name]({
-        handlebars: Handlebars
+            handlebars: Handlebars
         });
     });
 
@@ -27,12 +27,80 @@ module.exports = function (Handlebars) {
         return JSON.stringify(obj);
     });
 
-	Handlebars.registerHelper('toString', function (obj) {
+    Handlebars.registerHelper('toString', function (obj) {
         return obj.toString();
     });
 
     Handlebars.registerHelper('removeIndex', function (url) {
         return url.replace('index.html', '');
+    });
+
+    Handlebars.registerHelper('lookupCategory', function (obj, childPath) {
+        var chunks = childPath.split('.');
+        var count = 0;
+        var node = obj;
+        chunks.some(function (name) {
+            count++;
+            var fullCategoryName = chunks.slice(0, count).join('.');
+            var found = node.children.some(function (childNode) {
+                if (childNode.category == fullCategoryName) {
+                    node = childNode;
+                    return true;
+                }
+                return false;
+            });
+
+            if (!found) {
+                node = undefined;
+                return true;
+            }
+            return false;
+        });
+
+        return node;
+    });
+
+    /**
+     * Lookup nested object
+     */
+    Handlebars.registerHelper('lookupEx', function (obj, propertyPath) {
+        var props = propertyPath.split('.');
+        var current = obj;
+        while(props.length) {
+            if(typeof current !== 'object') return undefined;
+            current = current[props.shift()];
+        }
+        return current;
+    });
+
+    /**
+     * return array of category from root to leaf of @param {string} childPath
+     */
+    Handlebars.registerHelper('genBreadcrumb', function (obj, childPath) {
+        var chunks = childPath.split('.');
+        var count = 0;
+        var node = obj;
+        var ret = [];
+        chunks.some(function (name) {
+            count++;
+            var fullCategoryName = chunks.slice(0, count).join('.');
+            var found = node.children.some(function (childNode) {
+                if (childNode.category == fullCategoryName) {
+                    node = childNode;
+                    ret.push(childNode);
+                    return true;
+                }
+                return false;
+            });
+
+            if (!found) {
+                ret = undefined;
+                return true;
+            }
+            return false;
+        });
+
+        return ret;
     });
 
     Handlebars.registerHelper('formatDate', function (context, options) {
